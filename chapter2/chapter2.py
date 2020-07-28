dfb1e8a2802a8f4061d71a82a0466510c4f2f86d
AManKnowNothing <68155399+CyberBatMan2077@users.noreply.github.com>
2020-07-28 21:13:05 +0800

    第二章课后练习及学习笔记
    课后练习都在一个.py文件中，每题间已用注释隔开。
    学习笔记是我自己记录在OneNote中的，一并发出共同学习，因为我自身有其他语言的编程基础，所以记录得比较简单，仅供参考。

---
+++ chapter2/chapter2.py
@@ -0,0 +1,57 @@
+#orginal
+print("hello");
+
+
+
+#homework 2-1
+message = "Hello World "
+print(message)
+
+#homework 2-2
+a = "a1"
+print(a)
+a = "a2"
+print(a)
+
+#homework 2-3
+name = "Eric"
+print("Hello " + name + ", " + "would you like to learn some Python today?" )
+
+#homework 2-4
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+#homework 2-5
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+
+#homework 2-6
+famous_person = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+message = famous_person + " once said, " + quote
+print(message)
+
+#homework 2-7
+namespace = " Henry "
+print("\t",namespace)
+print("\t",namespace,"\n",namespace)
+print(namespace,namespace)
+print(namespace.lstrip())
+print(namespace.rstrip())
+print(namespace.strip())
+
+#homework 2-8
+print(5 + 3)
+print(2 * 4)
+print( 16 / 2)
+print(10 -2 )
+
+#homework 2-9
+fav_num = 3
+message = "my favorite number is " + str(fav_num)
+print(message)
+
+#homework 2-10 #HAHA
+
+#homework 2-11
+import this
